47b61941d52c630ffd632b70ad42e2464865f75b
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-24 11:59:57 -0700

    Configured webpack and service worker.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
-// TODO: Add CSS loaders and babel to webpack.
+// ----Configuring workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -26,16 +24,58 @@ module.exports = () => {
       // Webpack plugin that generates the html file and injects the bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'Quick Notes'
       }),   
       
-      
+      // Injects custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),      
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Quick Notes',
+        short_name: 'Notes',
+        description: 'Capture your thoughts with a quick note!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),      
       
     ],
 
+    // ---Adding CSS loaders and babel to webpack.
+
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          // babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },              
       ],
     },
   };
