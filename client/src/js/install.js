11fed51ea044dd4bccf3dd4233ddb809086985e0
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-24 14:07:16 -0700

    Configured install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,16 +1,40 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// ---Adds an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
-    // Store the triggered events
+    // Stores the triggered events
     window.deferredPrompt = event; 
-    
+
+    // Removes the hidden class from the button
+    butInstall.classList.toggle('hidden', false);
+
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// ---Implements a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return
+    }
+
+    //  Shows prompt
+    promptEvent.prompt();
+
+    // Resets the deferred prompt variable; it can only be used once
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+
+});
+
+// ---Adds a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+
+    // Clears prompt
+    window.deferredPrompt = null;
+
+});
