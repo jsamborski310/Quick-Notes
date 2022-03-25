f725b7f31413a59db385d65d17ab265977a92a12
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-24 22:02:37 -0700

    Updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -41,7 +41,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.add({content: content});
+  const request = store.put({content: content});
 
   const result = await request;
 
