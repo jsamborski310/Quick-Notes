41b1f5fe5b2f7645976383f1d35e33526c9779dc
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-24 21:53:50 -0700

    Testing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,6 +12,24 @@ const initdb = async () =>
     },
   });
 
+// ---Logic for a method that GETs all the content from the database
+export const getDb = async (content) => {
+
+  console.log('GET from the database');
+
+  const notesDb = await openDB('jate', 1);
+
+  const tx = notesDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+  return result;
+}
+
 // ---Logic to a method that accepts some content and adds it to the database (POST)
 export const putDb = async (content) => {
 
@@ -30,22 +48,5 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 
 };
-// ---Logic for a method that GETs all the content from the database
-export const getDb = async () => {
-
-  console.log('GET from the database');
-
-  const notesDb = await openDB('jate', 1);
-
-  const tx = notesDb.transaction('jate', 'readwrite');
-
-  const store = tx.objectStore('jate');
-
-  const request = store.getAll();
-
-  const result = await request;
-  console.log('result.value', result);
-  return result;
-}
 
 initdb();
